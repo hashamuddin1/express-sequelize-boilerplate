cd9f7fbbc1de21c4477de5d3bc201888e1f44ff8
hashamuddin1 <hashamlaptop@gmail.com>
2025-11-07 10:44:25 +0500

    add grafana

--- app.js
+++ app.js
@@ -4,11 +4,31 @@ const express = require('express');
 const { sequelize } = require('./models');
 const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
-const port=process.env.PORT
+const responseTime=require("response-time")
+const port=3002
+
+const client = require('prom-client'); //metric collection
+
+const collectDefaultMetric = client.collectDefaultMetrics;
+
+collectDefaultMetric({ register: client.register });
 
 const app = express();
 app.use(express.json());
 app.use('/user', userRoutes);
+
+app.get('/metrics',async (req,res)=>{
+  res.setHeader('Content-Type',client.register.contentType)
+  const metrics=await client.register.metrics()
+  res.send(metrics)
+})
+
+const reqResTime=new client.Histogram({
+  name:"http_express_req_res_time",
+  help:"this tell time"
+})
+
+// app.use()
 
 sequelize.sync().then(() => {
   app.listen(port, () => {
